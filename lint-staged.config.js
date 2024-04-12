c30c7e31ca61952bf162a6fdd0809d5e17bc8dfb
Noor Mohammed <noormohammedknr2@gmail.com>
2024-04-12 19:43:57 +0530

    feat: initial setup of the project

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "**/*.(js|jsx)": (filename) => [
+    `eslint --fix ${filename.join(" ")}`,
+    `prettier --write ${filename.join(" ")}`
+  ],
+
+  "**/*.(js|jsx)": () => "jsx --noEmail",
+
+  "**/*.(md|json)": (filename) => `prettier --write ${filename.join(" ")}`
+}
