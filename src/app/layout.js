85098e5789cc7f41eed1af43740866258469dd59
Noor Mohammed <noormohammedknr2@gmail.com>
2024-06-01 19:28:13 +0530

    Initial setup of the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Noor Portfolio",
+  title: "Noor Mohammed",
   description:
-    "This is the portfolio of Noor Mohammed, A full stack developer with ton of passion in web development",
+    "Hola, my name is Noor Mohammed. You can call me Noor. Currently Im working as full stack web developer as totally 1.5 year of experience. My tech stack is Next.Js, React.Js, Javascript, CSS, Tailwindcss, material UI, Node.Js, Mongo DB. I have good knowledge about the version controlling like github.",
 };
 
 export default function RootLayout({ children }) {
