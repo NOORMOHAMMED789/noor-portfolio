3a971b62afec494c8d0e509b565f9b428c0b140e
Noor Mohammed <noormohammedknr2@gmail.com>
2024-06-02 09:29:06 +0530

    header is in progress

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
 import React from "react";
 
 export default function DisplayStars() {
-  return <div></div>;
+  return (
+    <div className="mt-[2.4rem]">
+      <img
+        className=""
+        src="\Images\circle.png"
+        alt=""
+        width={16}
+        height={16}
+      />
+    </div>
+  );
 }
