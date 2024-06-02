025a33f0f9f8ea16bc523fff728660afb418e1ac
Noor Mohammed <noormohammedknr2@gmail.com>
2024-06-02 08:59:17 +0530

    installation of project and code setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import DisplayStars from "./Stars";
 
 export default function Header() {
-  return <div className="container">This is the header</div>;
+  return (
+    <div className="container">
+      <DisplayStars />
+    </div>
+  );
 }
