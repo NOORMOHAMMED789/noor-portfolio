3c8d3a79f4d04d8b99d005ca02b3a71967de9848
Noor <noor.mohammed@aprecomm.ai>
2025-01-18 09:02:45 +0530

    Header started with the text and formatting

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
+    <div className='py-4 px-2 md:py-6 md:px-4 lg:py-8 lg:px-6 bg-slate-500'>
       This is the header component
     </div>
   )
