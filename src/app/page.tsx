baf252e17c876a44266c81a011ea1f872e3b0c7f
Noor <noormohammedknr2@gmail.com>
2025-06-16 22:56:11 +0530

    Carousel removed due to amiguity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 // import Aboutme from "./components/Aboutme";
 // import Contactme from "./components/Contactme";
-import Carousel from "./components/Carousel";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 // import MyProjects from "./components/MyProjects";
 import { NavbarItems } from './constants/Navbar'
-import { brand, name } from "./constants/Ulity";
+import { name } from "./constants/Ulity";
 
 export default function Home() {
   return (
     <div>
-      <Carousel name={name} brand={brand}/>
       <Header NavbarItems={NavbarItems} />
       <Homepage name={name} />
       {/* <Aboutme name={name} /> */}
