79a7f5344a564a1b2d4eb03de466de5797ffc064
Noor <noormohammedknr2@gmail.com>
2025-06-17 23:51:35 +0530

    name is removed from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 // import MyProjects from "./components/MyProjects";
 import { NavbarItems } from './constants/Navbar'
-import { name } from "./constants/Ulity";
 
 export default function Home() {
   return (
     <div>
       <Header NavbarItems={NavbarItems} />
-      <Homepage name={name} />
+      <Homepage />
       {/* <Aboutme name={name} /> */}
       {/* <MyProjects name={name} /> */}
       {/* <Contactme name={name} /> */}
