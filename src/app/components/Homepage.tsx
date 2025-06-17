a3c4609f30823a6e11c0bb911b5e222bd0a3d166
Noor <noormohammedknr2@gmail.com>
2025-06-17 23:50:50 +0530

    removed image from homepage

--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-// import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-interface HomepageProps {
-  name: string;
-}
 
-const Homepage: React.FC<HomepageProps> = ({  }) => {
+const Homepage: React.FC = () => {
 
   const words = ["Full Stack Developer.", "Frontend Developer.", "Backend Developer.", "Mechanical Engineer too :)."];
   const [text, setText] = useState("");
@@ -31,28 +27,20 @@ const Homepage: React.FC<HomepageProps> = ({  }) => {
       } else {
         timer = setTimeout(() => setIsDeleting(true), pauseBetweenWords);
       }
+    } else if (letterIndex > 0) {
+      setText(currentWord.slice(0, letterIndex));
+      timer = setTimeout(() => setLetterIndex(letterIndex - 1), deletingSpeed);
     } else {
-      if (letterIndex > 0) {
-        setText(currentWord.slice(0, letterIndex));
-        timer = setTimeout(() => setLetterIndex(letterIndex - 1), deletingSpeed);
-      } else {
-        setIsDeleting(false);
-        setWordIndex((wordIndex + 1) % words.length);
-      }
+      setIsDeleting(false);
+      setWordIndex((wordIndex + 1) % words.length);
     }
+    
 
     return () => clearTimeout(timer);
   }, [letterIndex, isDeleting, wordIndex]);
 
   return (
     <div className="relative w-full" style={{ height: 'calc(100vh - 72px)' }}>
-      {/* <Image
-        src="http://ppcdn.500px.org/75319705/1991f76c0c6a91ae1d23eb94ac5c7a9f7e79c480/2048.jpg"
-        alt="homepage-bg"
-        fill
-        className="object-cover"
-        priority
-      /> */}
       <div>
         <div className="absolute inset-0 flex items-center justify-center bg-black/40">
           <h1 className="text-white text-3xl sm:text-5xl md:text-6xl font-bold text-center px-4">
