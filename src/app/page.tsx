5768642d6af727374e0e5727acc78847e8b23871
Noor <noormohammedknr2@gmail.com>
2025-06-19 23:30:26 +0530

    Cleaned up the page.tsx comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-// import Aboutme from "./components/Aboutme";
-// import Contactme from "./components/Contactme";
 import Header from "./components/Header";
-// import Homepage from "./components/Homepage";
-// import MyProjects from "./components/MyProjects";
 import { NavbarItems } from './constants/Navbar'
 
 export default function Home() {
   return (
     <div>
       <Header NavbarItems={NavbarItems} />
-      {/* <Homepage /> */}
-      {/* <Aboutme name={name} /> */}
-      {/* <MyProjects name={name} /> */}
-      {/* <Contactme name={name} /> */}
     </div>
   );
 }
