3747d8b40e5c6f4924dda4a21b2d809a316743e8
Noor <noormohammedknr2@gmail.com>
2025-06-20 17:06:00 +0530

    Added the navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const Header = ({  }) => {
   return (
     <nav className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-8xl px-2 sm:px-6 lg:px-6">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* <!-- Mobile menu button--> */}
@@ -31,7 +31,7 @@ const Header = ({  }) => {
               </svg>
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex flex-1 items-end justify-end sm:items-stretch sm:justify-end">
             
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
