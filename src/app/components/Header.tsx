f92443d746bb8b38c2c2e6a7e6f12bac4b54c436
Noor <noormohammedknr2@gmail.com>
2025-06-20 17:02:14 +0530

    Navbar styles with responsiveness

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,125 +1,64 @@
 'use client';
 import React from 'react';
 
-interface HeaderProps {
-  NavbarItems: string[];
-}
-
-const Header: React.FC<HeaderProps> = ({ NavbarItems }) => {
-  const commonClasses =
-    'underline decoration-[0.15em] decoration-slate-500 underline-offset-[0.2em] transition-[text-decoration-color,text-underline-offset,transform] hover:decoration-black duration-300 hover:underline-offset-[0.4em] hover:-translate-y-1 text-slate-500 hover:text-black';
 
 
+const Header = ({  }) => {
   return (
-    <div className='w-full flex flex-wrap justify-between items-center bg-[#C6E2FF] p-3 md:p-4 lg:p-6'>
-      <div className='container'>
-        <div className='text'>
-          <span>N</span>
-          <span>O</span>
-          <span>O</span>
-          <span>R</span>
-          <span>M</span>
-          <span>O</span>
-          <span>H</span>
-          <span>A</span>
-          <span>M</span>
-          <span>M</span>
-          <span>M</span>
-          <span>E</span>
-          <span>D</span>
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* <!-- Mobile menu button--> */}
+            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
+              <span className="absolute -inset-0.5"></span>
+              <span className="sr-only">Open main menu</span>
+              {/* <!--
+              Icon when menu is closed.
+
+              Menu open: "hidden", Menu closed: "block"
+          --> */}
+              <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              {/* <!--
+              Icon when menu is open.
+
+              Menu open: "block", Menu closed: "hidden"
+          --> */}
+              <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">About Me</a>
+                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">My Projects</a>
+                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Resume Download</a>
+                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+              </div>
+            </div>
+          </div>
+        
         </div>
       </div>
-      <nav className='hover:cursor-pointer hidden md:block lg:block'>
-        <ul className='flex justify-end items-end flex-row gap-4'>
-          {NavbarItems.map((nav, index) => (
-            <li
-              key={`${nav}_${index}`}
-              className={`${commonClasses}`}
-            >
-              {nav}
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <style>{`
-        .container {
-          width: 100%;
-          height: 100%;
-        }
-        
-        .text {
-          font-size: clamp(1rem, 1vw, 1rem);
-          letter-spacing: 6px;
-          color: #000;
-        }
-      
-        .text span {
-          display: inline-block;
-          animation: pop 1.6s ease-in-out infinite both;
-        }
 
-        .text span:nth-child(1) {
-          animation-delay: 0.4s;
-        }
+      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+          <a href="/aboutme" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">About Me</a>
+          <a href="/myprojects" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">My Projects</a>
+          <a href="/resume" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Resume Download</a>
+          <a href="/contact" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact Me</a>
+        </div>
+      </div>
+    </nav>
 
-        .text span:nth-child(2) {
-          animation-delay: 0.5s;
-        }
-
-        .text span:nth-child(3) {
-          animation-delay: 0.6s;
-        }
-
-        .text span:nth-child(4) {
-          animation-delay: 0.7s;
-        }
-
-        .text span:nth-child(5) {
-          animation-delay: 0.8s;
-        }
-        .text span:nth-child(6) {
-          animation-delay: 0.9s;
-        }
-        .text span:nth-child(7) {
-          animation-delay: 1s;
-        }
-        .text span:nth-child(8) {
-          animation-delay: 1.1s;
-        }
-        .text span:nth-child(9) {
-          animation-delay: 1.2s;
-        }
-        .text span:nth-child(10) {
-          animation-delay: 1.3s;
-        }
-        .text span:nth-child(11) {
-          animation-delay: 1.4s;
-        }
-        .text span:nth-child(12) {
-          animation-delay: 1.5s;
-        }
-        .text span:nth-child(13) {
-          animation-delay: 1.6s;
-        }
-
-        @keyframes pop {
-          0% {
-            transform: scale(1);
-            opacity: 1;
-          }
-          50% {
-            transform: scale(1.5);
-            opacity: 1;
-          }
-          100% {
-            transform: scale(1);
-            opacity: 1;
-          }
-        }
-
-      
-      `}</style>
-    </div>
   );
 
 };
