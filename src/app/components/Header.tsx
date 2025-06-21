c1ae5265e32b8914bf42a5793dfa691daf5cc1e7
Noor <noormohammedknr2@gmail.com>
2025-06-21 21:41:36 +0530

    Removed comments and aligned properly

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,33 +12,22 @@ const Header = () => {
       <div className="mx-auto max-w-8xl px-2 sm:px-6 lg:px-6">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* <!-- Mobile menu button--> */}
             <button onClick={() => setShowMenu(!showMenu)} type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
-              {/* <!--
-              Icon when menu is closed.
-
-              Menu open: "hidden", Menu closed: "block"
-          --> */}
               <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
-              {/* <!--
-              Icon when menu is open.
-
-              Menu open: "block", Menu closed: "hidden"
-          --> */}
               <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
           <div className="flex flex-1 items-end justify-end sm:items-stretch sm:justify-end">
-            
+
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+
                 <a href="/aboutme" className={`${commonClass}`} aria-current="page">About Me</a>
                 <a href="/projects" className={`${commonClass}`} aria-current='page'>My Projects</a>
                 <a href="/resume" className={`${commonClass}`} aria-current='page'>Resume Download</a>
@@ -46,14 +35,12 @@ const Header = () => {
               </div>
             </div>
           </div>
-        
+
         </div>
       </div>
 
-      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
       {showMenu && <div className="sm:hidden" id="mobile-menu">
         <div className="space-y-1 px-2 pt-2 pb-3">
-          {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
           <a href="/aboutme" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">About Me</a>
           <a href="/myprojects" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">My Projects</a>
           <a href="/resume" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Resume Download</a>
