ae87a24e660b0bf838179f98de0bf62f85d3afd4
Noor Mohammed <noormohammedknr2@gmail.com>
2025-07-22 23:02:01 +0530

    homepage first portion is completed

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,12 +1,21 @@
 
 const HomePage: React.FC = () => {
     return (
-        <div className="">
-            <strong className="text-[2.4rem] font-Preahvihear">Hello. Im Noor Mohammed</strong>
-            <blockquote className="text-[1.6rem]">Software developer</blockquote>
-            <span className="text-[1.6rem]">
-            As a Associate Member of Technical Staff at Aprecomm, with a bachelor of technology in Mechanical Engineering from Madanapalle Institute of Technology & Science, I bring a unique perspective to the tech industry. I am passionate about learning new technologies and improving my coding skills.I am always eager to take on new challenges and collaborate with diverse teams.
-            </span>
+        <div>
+            <header>
+                <h1 className="homepage_title">Hello. Im Noor Mohammed</h1>
+                <p className="homepage_tag">Software Developer</p>
+            </header>
+            <section className="homepage_desc">
+                <p>
+                    As an Associate Member of Technical Staff at Aprecomm, with a Bachelor of Technology in Mechanical Engineering
+                    from Madanapalle Institute of Technology & Science, I bring a unique perspective to the tech industry.
+                    I am passionate about learning new technologies and improving my coding skills.
+                </p>
+                <p className="">
+                    I am always eager to take on new challenges and collaborate with diverse teams.
+                </p>
+            </section>
         </div>
     )
 }
